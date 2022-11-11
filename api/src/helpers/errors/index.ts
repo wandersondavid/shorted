425cdd55fdf7a8861b169eeae897ee0b026a1cb4
Wanderson David <waandersondavid.ifg@gmail.com>
2022-11-11 00:13:31 -0300

    feat: melhorando tratamento de error

---
+++ api/src/helpers/errors/index.ts
@@ -0,0 +1,45 @@
+export type DefaultErrorInput = {
+  name: string
+  code: number
+  message: string
+}
+
+export class DefaultError extends Error {
+  code: number
+
+  constructor ({ name, code, message }: DefaultErrorInput) {
+    super(message)
+    this.name = name
+    this.code = code
+  }
+}
+
+export class AuthError extends DefaultError {
+  constructor (message: string = 'Unauthorized') {
+    super({ name: 'AuthError', code: 401, message })
+  }
+}
+
+export class ForbiddenError extends DefaultError {
+  constructor (message: string) {
+    super({ name: 'ForbiddenError', code: 403, message })
+  }
+}
+
+export class NotFoundError extends DefaultError {
+  constructor (message: string) {
+    super({ name: 'NotFoundError', code: 404, message })
+  }
+}
+
+export class ValidationError extends DefaultError {
+  constructor (message: string) {
+    super({ name: 'ValidationError', code: 422, message })
+  }
+}
+
+export class UnknownError extends DefaultError {
+  constructor () {
+    super({ name: 'UnknownError', code: 418, message: 'Unknown error' })
+  }
+}
--- api/src/ports/express/modules/error.ts
+++ api/src/ports/express/modules/error.ts
@@ -1,12 +1,21 @@
+import { DefaultErrorInput, NotFoundError } from "@/helpers/errors";
 import { Response, Request } from "express";
 
-const HandlerErrorNotFound = (_req: Request, _res: Response, next: any) => {
-    next('Not Found')
+const HandlerErrorNotFound = (_req: Request, _res: Response, _next: any) => {
+
+    throw new NotFoundError('Not Found')
 }
 
-const HandlerError = (err: Error,_req: Request, res: Response, _next: any) => {
-    res.status(500)
-    res.json({ error: err })
+const HandlerError = (err: DefaultErrorInput,_req: Request, res: Response, _next: any) => {
+
+    res.status(err?.code || 500)
+
+    res.json({ error: {
+            code: err.code,
+            name: err.name,
+            message: err.message
+        }
+    })
 }
 
 export { HandlerError, HandlerErrorNotFound };
