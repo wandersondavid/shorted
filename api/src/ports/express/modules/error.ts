487dbe9b63ed0b68d1ecd68e525b445e48981c61
Wanderson David <waandersondavid.ifg@gmail.com>
2022-11-12 01:36:49 -0300

    feat: add tratamento de timeout

--- api/src/ports/express/modules/error.ts
+++ api/src/ports/express/modules/error.ts
@@ -2,31 +2,41 @@ import { DefaultErrorInput, NotFoundError } from "@/helpers/errors";
 import { Response, Request } from "express";
 
 const HandlerErrorNotFound = (_req: Request, _res: Response, _next: any) => {
+  throw new NotFoundError("Not Found");
+};
 
-    throw new NotFoundError('Not Found')
-}
+const HandlerError = ( err: DefaultErrorInput,  req: Request,  res: Response,  _next: any) => {
+  try {
+    res.status(err?.code || 500);
 
-const HandlerError = (err: DefaultErrorInput,_req: Request, res: Response, _next: any) => {
-
-    res.status(err?.code || 500)
-
-    try {
-
-        res.json({ error: {
-                code: err.code,
-                name: err.name,
-                message: err.message,
-            }
-        })
-
-    } catch (err) {
-        res.json({ error: {
-                code: 555,
-                name: 'Error Interno',
-                message: 'Error Interno'
-            }
-        })
+    return res.json({
+      error: {
+        code: err.code,
+        name: err.name,
+        message: err.message,
+      },
+    });
+  } catch (err: unknown) {
+    if (req.timedout) {
+      res.status(503);
+      return res.send({
+        error: {
+          code: 503,
+          name: "Timeout",
+          message: "timedout",
+        },
+      });
     }
-}
+
+    res.status(500);
+    return res.send({
+      error: {
+        code: 500,
+        name: "Internal error",
+        message: "Internal error",
+      },
+    });
+  }
+};
 
 export { HandlerError, HandlerErrorNotFound };
--- api/src/ports/express/sever.ts
+++ api/src/ports/express/sever.ts
@@ -1,18 +1,25 @@
 import express from "express";
 import cors from "cors";
-import { HandlerError,  HandlerErrorNotFound,  shortUrlRoutes } from "./modules";
+import {
+  HandlerError,
+  HandlerErrorNotFound,
+  shortUrlRoutes,
+} from "./modules";
+import timeout from "connect-timeout";
 
-const app = express();
 const PORT = process.env["PORT"];
+const app = express();
 
+app.use(timeout("60s"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/api/v1", shortUrlRoutes);
 
-app.use(HandlerErrorNotFound)
-app.use(HandlerError)
+app.use("/api/v1", shortUrlRoutes);
 
 app.listen(PORT, () => {
   console.log(`Sever is listenin on port ${PORT}`);
-}).setTimeout(20000);
+});
+
+app.use(HandlerErrorNotFound);
+app.use(HandlerError);
