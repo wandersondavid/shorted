bedfb97a0f859e58ac25b6be32e448abd97fa736
wandersondavid <wandersondavid.ifg@gmail.com>
2023-02-10 23:35:12 -0300

    refactor: refactoring helpes erros

--- api/src/helpers/errors/index.ts
+++ api/src/helpers/errors/index.ts
@@ -1,45 +1,28 @@
 export type DefaultErrorInput = {
   name: string
   code: number
-  message: string
+  message?: string
 }
 
 export class DefaultError extends Error {
   code: number
 
-  constructor ({ name, code, message }: DefaultErrorInput) {
+  constructor({ name, code, message }: DefaultErrorInput) {
     super(message)
     this.name = name
     this.code = code
   }
 }
 
-export class AuthError extends DefaultError {
-  constructor (message: string = 'Unauthorized') {
-    super({ name: 'AuthError', code: 401, message })
-  }
-}
+const createError = ({ name, code, message }: DefaultErrorInput) =>
+  class extends DefaultError {
+    constructor(messagen?: string) {
+      super({ name, code, message: message || messagen });
+    }
+  };
 
-export class ForbiddenError extends DefaultError {
-  constructor (message: string) {
-    super({ name: 'ForbiddenError', code: 403, message })
-  }
-}
-
-export class NotFoundError extends DefaultError {
-  constructor (message: string) {
-    super({ name: 'NotFoundError', code: 404, message })
-  }
-}
-
-export class ValidationError extends DefaultError {
-  constructor (message: string) {
-    super({ name: 'ValidationError', code: 422, message })
-  }
-}
-
-export class UnknownError extends DefaultError {
-  constructor () {
-    super({ name: 'UnknownError', code: 418, message: 'Unknown error' })
-  }
-}
+export const AuthError = createError({ name:'AuthError', code:401, message:'Unauthorized' });
+export const ForbiddenError = createError({ name:'ForbiddenError', code:403, message:'' });
+export const NotFoundError = createError({ name:'NotFoundError', code:404, message:'' });
+export const ValidationError = createError({ name:'ValidationError', code:422, message:'' });
+export const UnknownError = createError({ name:'UnknownError', code:418, message:'Unknown error' });
