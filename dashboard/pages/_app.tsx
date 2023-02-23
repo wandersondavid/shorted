f6fa0d030cc72e562d44875f832373319ed83fe1
wandersondavid <wandersondavid.ifg@gmail.com>
2023-02-22 22:16:50 -0300

    feat: auth supabase

--- dashboard/pages/_app.tsx
+++ dashboard/pages/_app.tsx
@@ -4,9 +4,23 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import { darkTheme, lightTheme } from "../theme.config";
 import { useEffect, useState } from "react";
 import { AuthProvider } from "../hooks/auth";
-export default function MyApp({ Component, pageProps }: AppProps) {
+
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
+const NEXT_PUBLIC_SUPABASE_URL:string = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const NEXT_PUBLIC_SUPABASE_KEY:string = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
+
+
+export default function MyApp({ Component, pageProps }:  AppProps<{
+  initialSession: Session,
+}>) {
   // globalStyles();
+
+  const [supabase] = useState(() => createBrowserSupabaseClient({supabaseUrl:NEXT_PUBLIC_SUPABASE_URL , supabaseKey:NEXT_PUBLIC_SUPABASE_KEY }))
+
+
     return (
+      <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
       <NextThemesProvider
         defaultTheme="system"
         attribute="class"
@@ -21,5 +35,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           </AuthProvider>
         </NextUIProvider>
       </NextThemesProvider>
+      </SessionContextProvider>
     );
 }
